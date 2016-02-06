982f41a7066ccd3f8ed783cd13b175c7150f4328
dfreire <dario.freire@gmail.com>
2016-02-06 10:56:02 +0000

    first commit

---
+++ src/datastore/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+
+	"labix.org/v2/mgo"
+)
+
+func main() {
+
+	session, err := mgo.Dial("127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+
+	e := echo.New()
+	e.Static("/", "public")
+	e.Run(":3050")
+}
