c52bbc6ad52ca319cc3f96fb30b946c350b65263
dfreire <dario.freire@gmail.com>
2016-02-23 21:29:40 +0000

    update

--- main_test.go
+++ main_test.go
@@ -1,11 +1,21 @@
 package main_test
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/verdverm/frisby"
 )
 
 func TestSomething(t *testing.T) {
-	assert.False(t, false)
+	errs := frisby.Create("Test ping").
+		Get("http://localhost:5025/ping").
+		Send().
+		ExpectStatus(http.StatusOK).
+		Errors()
+
+	for _, err := range errs {
+		assert.Nil(t, err)
+	}
 }
